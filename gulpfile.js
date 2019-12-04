720ff91e32ec72c5a4777ec59df3c0b72a250334
Андрей <fakt37@gmail.com>
2019-12-04 19:04:55 +0300

    feat: add normalize, griid, interface,  variables, header styles in template folser

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 const {src, dest, watch}        = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass        = require('gulp-sass');
-const cssmin = require('gulp-cssmin');
-const rename = require('gulp-rename');
+
 // Static server
 function bs() {
     servSass ();
@@ -16,17 +15,11 @@ function bs() {
     watch("./js/*.js").on('change', browserSync.reload);
 };
 
-function min() {
-    src('./css/*.css')
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(dest('css'));
-};
 
 function servSass() {
     return src("./sass/*.sass")
         .pipe(sass())
-        .pipe(dest("/css"))
+        .pipe(dest("css"))
         .pipe(browserSync.stream());
 };
 exports.serv = bs;
